05acbd589dfacab9ac96d0828f5978db23be8511
宛天 <zhaobin.zb@alibaba-inc.com>
2018-12-17 21:14:19 +0800

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const less = require('gulp-less');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
+const replace = require('gulp-replace');
+ 
+gulp.task('less', () => gulp.src('src/*.less')
+  .pipe(less())
+  .pipe(gulp.dest('lib')));
 
-gulp.task('less', function(){
-  return gulp.src('src/*.less')
-    .pipe(gulp.dest('lib'))
+gulp.task('autoprefixer', () => gulp.src('src/*.css')
+  .pipe(sourcemaps.init())
+  .pipe(postcss([autoprefixer()]))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('./lib')));
+
+gulp.task('replace', () => {
+  gulp.src('lib/*.js')
+    .pipe(replace(/\/(.+)\.less/, '/$1.css'))
+    .pipe(replace(/\/(.+)\.styl/, '/$1.css'))
+    .pipe(gulp.dest('lib'));
 });
 
-gulp.task('stylus', function(){
-  return gulp.src('src/*.styl')
-  .pipe(gulp.dest('lib'))
-});
-
-gulp.task('default', [ 'less', 'stylus' ]);
+gulp.task('default', ['less', 'autoprefixer', 'replace']);
