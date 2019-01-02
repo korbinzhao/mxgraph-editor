2bf21409729acb7f11ba898d7b0f1f0e49a4c43a
宛天 <zhaobin.zb@alibaba-inc.com>
2019-01-02 17:18:15 +0800

    update shape config in utils

--- demo/shape-config/card-shape.js
+++ demo/shape-config/card-shape.js
@@ -1,17 +1,19 @@
-// export default {
-//   fujiashebei: 'https://img.alicdn.com/tfs/TB1ejUeiAPoK1RjSZKbXXX1IXXa-36-32.png',
-//   chanchuwu: 'https://img.alicdn.com/tfs/TB1ht.aisbpK1RjSZFyXXX_qFXa-32-32.png',
-//   shengchanhuanjie: 'https://img.alicdn.com/tfs/TB1LdMbiAvoK1RjSZFwXXciCFXa-32-32.png',
-//   wuliao: 'https://img.alicdn.com/tfs/TB17Zu0lQzoK1RjSZFlXXai4VXa-128-128.png',
-//   yuanliao: 'https://img.alicdn.com/tfs/TB1dn6GlMDqK1RjSZSyXXaxEVXa-200-200.png',
-//   cangku: 'https://img.alicdn.com/tfs/TB1dn6GlMDqK1RjSZSyXXaxEVXa-200-200.png',
-// };
-
-
 export default [{
   name: '主设备',
   key: 'zhushebei',
   logo: 'https://img.alicdn.com/tfs/TB1eD9LdgHqK1RjSZJnXXbNLpXa-144-128.png',
+  width: 100,
+  height: 80
+}, {
+  name: '附加设备',
+  key: 'fujiashebei',
+  logo: 'https://img.alicdn.com/tfs/TB1ejUeiAPoK1RjSZKbXXX1IXXa-36-32.png',
+  width: 100,
+  height: 80
+}, {
+  name: '产出物',
+  key: 'chanchuwu',
+  logo: 'https://img.alicdn.com/tfs/TB1ht.aisbpK1RjSZFyXXX_qFXa-32-32.png',
   width: 100,
   height: 80
 }];
